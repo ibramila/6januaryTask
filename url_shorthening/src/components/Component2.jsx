e054b653970de4f933ad2a8829b739aa38bb544f
iammmila <ibramila2002@gmail.com>
2023-01-06 11:56:45 +0400

    fullPage/ positions

--- url_shorthening/src/components/Component2.jsx
+++ url_shorthening/src/components/Component2.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import "./../assets/scss/_component2.scss"
 function Component2() {
+
+    
     return (
-        <section>
-            <div className='section2'></div>
+        <section className='sect2'>
+            <div className='section2'>
+                <form action="">
+                    <input type="text" placeholder="Enter Name" required />
+                    <input type="text" placeholder="Enter Description" required />
+                    <button>Shorten it!</button>
+                </form>
+            </div>
         </section>
     )
 }
